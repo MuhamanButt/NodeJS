fab9ccbd77cc01b88cd534e6208ec148fdd2c0a1
muhaman <muhamanibutt@gmail.com>
2024-01-26 18:10:15 +0500

    Lecture 27

--- index.js
+++ index.js
@@ -1,28 +1,19 @@
 //! index.js
-
-const fs = require("node:fs");
+const fs = require("node:fs/promises");
 
 console.log("First");
-let fileContents = fs.readFileSync("./file.txt");
-console.log(fileContents);
-fileContents = fs.readFileSync("./file.txt", "utf-8");
-console.log(fileContents);
+fs.readFile("file.txt", "utf-8")
+  .then((data) => console.log(data))
+  .catch((error) => console.log(error));
 console.log("Second");
-//here sync means that the js engine will wait till the data is read before moving to next line
 
-fs.readFile("./file.txt", "utf-8", (error, data) => {
-  if (error) console.log(error);
-  else console.log(data);
-});
-console.log("Third");
+async function readFile() {
+  try {
+    const data = await fs.readFile("greet.txt", "utf-8");
+    console.log(data);
+  } catch (error) {
+    console.log(error);
+  }
+}
 
-fs.writeFileSync("./greet.txt", "Hello world");
-fs.writeFile("./greet.txt", "Hello Muhaman", (error) => {
-  if (error) console.log(error);
-  else console.log("File written");
-});
-
-fs.writeFile("./greet.txt", " Hello Ali", { flag: "a" }, (error) => {
-  if (error) console.log(error);
-  else console.log("File written");
-});
+readFile()
