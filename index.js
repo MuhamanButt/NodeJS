84bde4814e58dcd6d1398886a6de59376b1ba659
muhaman <muhamanibutt@gmail.com>
2024-01-26 19:05:37 +0500

    Lecture 35

--- index.js
+++ index.js
@@ -4,18 +4,27 @@ const http = require("node:http");
 const fs = require("node:fs");
 
 const server = http.createServer((req, res) => {
-  res.writeHead(200, { "Content-Type": "text/html" });
-
-  fs.createReadStream(__dirname + "/index.html").pipe(res)
-
-//   fs.createReadStream("./index.html").pipe(res)
-
-//   const html = fs.readFileSync("./index.html", "utf-8");
-//   res.end(html);
+    //req.method GET POST PUT DELETE
+  if (req.url === "/") {
+    res.writeHead(200, { "Content-Type": "text/plain" });
+    res.end("Home page");
+  } else if (req.url === "/about") {
+    res.writeHead(200, { "Content-Type": "text/plain" });
+    res.end("About page");
+  } else if (req.url === "/api") {
+    res.writeHead(200, { "Content-Type": "application/json" });
+    res.end(
+      JSON.stringify({
+        firstName: "Muhaman",
+        lastName: "Ijaz",
+      })
+    );
+  } else {
+    res.writeHead(404);
+    res.end("Page not Found");
+  }
 });
 
 server.listen(3000, () => {
   console.log("server running on port 3000");
 });
-
-//req gives information about incoming requests and res is used to send back response to client and we specify that server must listen to incoming requests on port 3000
