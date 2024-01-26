ddfabc09eae65a26354e128e33d61e8b0bec6996
muhaman <muhamanibutt@gmail.com>
2024-01-26 18:27:50 +0500

    Lecture 29

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 //! index.js
 
+const zlib=require("node:zlib")
 const fs = require("node:fs");
+
+const gzip=zlib.createGzip();
+
 const readableStream = fs.createReadStream("./file.txt", {
   encoding: "utf-8",
   highWaterMark:2
@@ -8,7 +12,8 @@ const readableStream = fs.createReadStream("./file.txt", {
 
 const writeableStream = fs.createWriteStream("./file2.txt");
 
-readableStream.on("data", (chunk) => {
-  console.log(chunk);
-  writeableStream.write(chunk);
-});
+readableStream.pipe(writeableStream)
+
+//creates a zip folder of type gzip
+readableStream.pipe(gzip).pipe(fs.WriteStream("./file2.txt.gz"))
+//readableStream->gzip->writestream
