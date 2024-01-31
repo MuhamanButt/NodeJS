39d7c0505de2d0165b8ab79a45058dba62a3e7e6
muhaman <muhamanibutt@gmail.com>
2024-01-31 19:58:09 +0500

    lexture 42

--- index.js
+++ index.js
@@ -1,11 +1,15 @@
 //! index.js
+﻿
 
-const crypto = require("node:crypto");
+// Event Loop - Execution Order
+// 1. Any callbacks in the micro task queues are executed. First, tasks in the nextTick queue and only then tasks in the promise queue
+// 2. All callbacks within the timer queue are executed
+// 3. Callbacks in the micro task queues if present are executed. Again, first tasks in the nextTick queue and then tasks in the promise queue
+// 4. All callbacks within the I/O queue are executed
+// 5. Callbacks in the micro task queues if present are executed. nextTick queue followed by Promise queue.
+// 6. All callbacks in the check queue are executed
+// 7. Callbacks in the micro task queues if present are executed. Again, first tasks in the nextTick queue and then tasks in the promise queue
+// 8. All callbacks in the close queue are executed
+// 9. For one final time in the same loop, the micro task queues are executed. nextTick queue followed by promise queue.
 
-const MAX_CALLS = 3;
-const start = Date.now();
-for (let i = 0; i < MAX_CALLS; i++) {
-  crypto.pbkdf2("password", "salt", 100000, 512, "sha512", () => {
-    console.log(`Hash : ${i + 1}`, Date.now() - start);
-  });
-}
+
