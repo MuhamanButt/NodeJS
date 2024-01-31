de944b0ba387d4b23aa580499058003f8766981e
muhaman <muhamanibutt@gmail.com>
2024-01-31 21:21:02 +0500

    lexture 45

--- index.js
+++ index.js
@@ -10,31 +10,16 @@
 // 8. All callbacks in the close queue are executed
 // 9. For one final time in the same loop, the micro task queues are executed. nextTick queue followed by promise queue.
 
-setTimeout(() => {
-  console.log("this is setTimerout 1");
-}, 0);
-setTimeout(() => {
-  console.log("this is setTimerout 2");
-  Promise.resolve().then(() => console.log("this is promise.resolve inside setTimeout"));
-  process.nextTick(() => console.log("this is nextTick inside setTimeout"));
-}, 0);
-setTimeout(() => {
-  console.log("this is setTimerout 3");
-}, 0);
 
-process.nextTick(() => console.log("this is process.NextTick 1"));
-process.nextTick(() => {
-  console.log("this is process.NextTick 2");
-  process.nextTick(() =>
-    console.log("this is inner next tick inside next tick")
-  );
-});
-process.nextTick(() => console.log("this is process.NextTick 3"));
 
-Promise.resolve().then(() => console.log("this is promise.resolve 1"));
-Promise.resolve().then(() => {
-  console.log("this is Promise.resolve 2");
-  process.nextTick(() =>
-    console.log("this is the inner next tik inside promise then block")
-  );
-});
+
+const fs = require("fs")
+fs.readFile(__filename,()=>{
+  console.log("this is readFile 1")
+})
+process.nextTick(()=>console.log('this is process.nextTick 1'))
+Promise.resolve().then(()=>console.log('this is promise.resolve 1'))
+setTimeout(() => {
+  console.log('this is setTimeout 1')
+}, 0);
+for(let i=0;i<2000000;i++){}
